1ab7bc6d5eff678a37529357e10a9f3f2dcb5406
Adam Blackburn <regality@gmail.com>
2012-11-03 12:01:57 -0600

    cleaning up and fixing pid bug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var cluster = require('cluster')
   , os = require('os')
   , undeadCount = 0
-  , children;
+  , children = [];
 
 exports.start = function(workers, basedir) {
   if (cluster.isMaster) {
@@ -13,36 +13,35 @@ exports.start = function(workers, basedir) {
   }
 };
 
-var runMaster = function(workers, basedir) {
-  var cpus = os.cpus().length
-    , count
-    , autoRestart;
-  children = [];
-  for (var app in workers) {
-    if (workers.hasOwnProperty(app)) {
-      // get the number of workers to spwan
-      if (typeof workers[app] === "object") {
-        eval("count = " + workers[app]["count"]);
-        autoRestart = workers[app]["autoRestart"];
-      } else {
-        eval("count = " + workers[app]);
-        autoRestart = true;
-      }
-      console.log("Spawning " + count + " " + app + " workers.");
-      // launch them
-      for (var i = 0; i < count; i++) {
-        spawnWorker(basedir + "/" + app, autoRestart);
-      }
+function runMaster(workers, basedir) {
+
+  Object.keys(workers).forEach(function(app) {
+    var count
+      , autoRestart = true;
+    // get the number of workers to spwan
+    if (typeof workers[app] === "object") {
+      count = getCount(workers[app].count);
+      autoRestart = workers[app].autoRestart;
+    } else {
+      count = getCount(workers[app]);
     }
-  }
+
+    console.log("Spawning " + count + " " + app + " workers.");
+    // launch them
+    for (var i = 0; i < count; i++) {
+      spawnWorker(basedir + "/" + app, autoRestart);
+    }
+  });
+
   process.addListener('SIGTERM', function() {
     for (var i = 0; i < children.length; ++i) {
       children[i].kill();
     }
   });
+
 };
 
-var runWorker = function() {
+function runWorker() {
   // workers wait to hear from the server to know what to do
   process.once("message", function(app) {
     // start the app
@@ -50,32 +49,40 @@ var runWorker = function() {
   });
 };
 
-var spawnWorker = function(app, autoRestart) {
+function spawnWorker(app, autoRestart) {
   // autoRestart defaults to true
   autoRestart = (typeof autoRestart === "boolean" ? autoRestart : true);
   var worker = cluster.fork()
-    , index;
+
   // tell the worker what to do
   worker.send(app);
-  console.log("worker started with pid " + worker.pid);
+  console.log("worker started with pid " + worker.process.pid);
   ++undeadCount;
-  index = children.length;
   children.push(worker);
+
   worker.on('exit', function(code, signal) {
     if (signal) {
-      console.log(app + " worker " + worker.pid + " died with signal " + signal + ".");
+      console.log(app + " worker " + worker.process.pid + " died with signal " + signal + ".");
     } else if (code) {
-      console.log(app + " worker " + worker.pid + " died with code " + code + ".");
+      console.log(app + " worker " + worker.process.pid + " died with code " + code + ".");
     } else {
-      console.log(app + " worker " + worker.pid + " died.");
+      console.log(app + " worker " + worker.process.pid + " died.");
     }
     --undeadCount;
+
     if (undeadCount == 0) {
       console.log("No workers remaining, commiting suicide.");
       process.exit();
     }
-    children.splice(index, 1);
+
+    for (var i = 0; i < children.length; ++i) {
+      if (children[i] === worker) {
+        children.splice(i, 1);
+        break;
+      }
+    }
   });
+
   if (autoRestart) {
     undeadCount = Infinity;
     // auto restart workers on death
@@ -85,4 +92,16 @@ var spawnWorker = function(app, autoRestart) {
     });
   }
 };
+
+function getCount(n) {
+  var cpus = os.cpus().length
+  var match;
+  if (n === 'cpus') {
+    return cpus;
+  }
+  if (match = n.toString().match(/cpus\s*\*\s*(\d+)/)) {
+    return cpus * parseInt(match[1]);
+  }
+  return parseInt(n);
+}
 
